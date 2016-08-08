1b418a790ada20bc89e53a208a0f8494de08e1f1
Michael Mairegger <michael.mairegger@yahoo.de>
2016-08-08 16:08:58 +0200

    Rename Testing libraries.

--- Mairegger.Printing.Test/Properties/AssemblyInfo.cs
+++ Mairegger.Printing.Test/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mairegger.Printing.Test")]
+[assembly: AssemblyTitle("Mairegger.Printing.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
--- Mairegger.Printing/Properties/AssemblyInfo.cs
+++ Mairegger.Printing/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Mairegger.Printing.Test")]
+[assembly: InternalsVisibleTo("Mairegger.Printing.Tests")]
