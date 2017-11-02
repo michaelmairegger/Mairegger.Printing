7dc98504aebc5434391f7ce1959d4b2795d72331
Michael Mairegger <michael.mairegger@yahoo.de>
2017-11-02 13:39:18 +0100

    Add code generation script

--- scripts/generate-documentation.bat
+++ scripts/generate-documentation.bat
@@ -1,4 +1,1 @@
-@ECHO OFF
-PUSHD %~dp0
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command ".\generate-documentation.ps1 %*; exit $LastExitCode;"
-POPD
+PowerShell -file "generate-documentation.ps1" %*
--- scripts/generate-documentation.ps1
+++ scripts/generate-documentation.ps1
@@ -1,8 +1,35 @@
-﻿$currentDirectory = [string](Get-Item -Path '.');
+﻿$docFolder = "_site"
+$documentationBranch = "documentation" #TODO change to gh-pages
 
-if($currentDirectory.EndsWith("scripts"))
+Push-Location -StackName ScriptFolder
+Set-Location ../$docFolder
+
+If(!(Test-Path -Path $docFolder))
 {
-    Set-Location ".."
+    git clone https://github.com/xxMUROxx/Mairegger.Printing . -b $documentationBranch
+}
+else
+{
+    $currentBranch= &git rev-parse --abbrev-ref HEAD 
+
+    if($currentBranch -ne $documentationBranch)
+    {
+        git checkout $documentationBranch
+    }
+    git pull
 }
 
-cmd /c docfx
+Set-Location ..
+
+docfx
+
+Set-Location $docFolder
+
+Remove-Item manifest.json
+Get-ChildItem * -Include *.yml | Remove-Item
+
+git add .
+git commit -m "Update $documentationBranch"
+git push
+
+Pop-Location -StackName ScriptFolder
