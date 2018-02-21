0daa579b02fbd1bd73d8db89860e64354072c1cd
Michael Mairegger <michael.mairegger@yahoo.de>
2018-02-21 10:15:42 +0100

    Build 1.3.1

--- Mairegger.Printing/Properties/AssemblyInfo.cs
+++ Mairegger.Printing/Properties/AssemblyInfo.cs
@@ -17,37 +17,16 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Mairegger.Printing")]
 [assembly: AssemblyDescription("Printing library for WPF")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Mairegger.Printing")]
-[assembly: AssemblyCopyright("Copyright 2015 Michael Mairegger")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCopyright("Copyright 2016-2018 Michael Mairegger")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0a7f0fd0-f7f4-415e-8494-e1ca351ef82f")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
+[assembly: AssemblyFileVersion("1.3.1")]
 [assembly: InternalsVisibleTo("Mairegger.Printing.Tests")]
